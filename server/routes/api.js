66dca0f9c92b225388128b0b88d6eae85abba835
Rania Haj <haj.rania@hotmail.com>
2023-03-14 15:45:40 +0200

    new project

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,18 +5,30 @@ const app = express();
 const Expense = require("../model/Expense");
 
 router.get("/expenses", function (req, res) {
-  Expense.find({})
-    .sort({ date: "asc" })
-    .then(function (expenses) {
+  const d1 = moment(req.query.d1).format("YYYY-MM-DD");
+  const d2 = req.query.d2
+    ? moment(req.query.d2, "YYYY-MM-DD")
+    : moment().format("YYYY-MM-DD");
+  if (d1) {
+    Expense.find({
+      $and: [{ date: { $gte: d1 } }, { date: { $lt: d2 } }],
+    })
+      .sort({ date: "asc" })
+      .then((expenses) => {
+        res.send(expenses);
+      });
+  } else {
+    Expense.find({}).then((expenses) => {
       res.send(expenses);
     });
+  }
 });
 
 router.post("/expense", function (req, res) {
   let e = new Expense(req.body);
   let date = req.body.date
-    ? moment(req.body.date).format("LLLL")
-    : moment().format("LLLL");
+    ? moment(req.body.date, "YYYY-MM-DD")
+    : moment().format("YYYY-MM-DD");
   e.date = date;
   e.save().then(function () {
     console.log(`amount of Expense: ${e.amount} we spent money on ${e.item}`);
@@ -24,16 +36,42 @@ router.post("/expense", function (req, res) {
   res.status(201).send(e);
 });
 
-router.put("/update/:group1/:group2", function (req, res) {
+router.put("/update", function (req, res) {
   Expense.findOneAndUpdate(
-    { group: req.params.group1 },
-    { group: req.params.group2 }
+    { group: req.body.group1 },
+    { $set: { group: req.body.group2 } }
   )
     .then(function (expense) {
-      res.send(`Expense ${expense.item} updated to group ${req.params.group2}`);
+      res.send(`Expense ${expense.item} updated to group ${req.body.group2}`);
     })
     .catch((err) => {
       res.status(500).send(err);
     });
+});
+
+router.get("/expenses/:group", function (req, res) {
+  const total = req.query.total;
+  const group = req.params.group;
+  if (total === "true") {
+    Expense.aggregate([
+      {
+        $match: { group: group },
+      },
+      {
+        $group: {
+          _id: "$group",
+          total: { $sum: "$amount" },
+        },
+      },
+    ]).then(function (total) {
+      res.send(total);
+    });
+  } else {
+    Expense.find({
+      group: group,
+    }).then(function (expenses) {
+      res.send(expenses);
+    });
+  }
 });
 module.exports = router;
